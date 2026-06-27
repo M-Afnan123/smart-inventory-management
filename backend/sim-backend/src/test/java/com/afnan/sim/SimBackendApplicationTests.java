d83ccb810e53b7e6485d8020b1459f26407f6319
mohdxafnan <mohammeddafnan557@gmail.com>
2026-06-27 20:52:18 +0530

    feat(SCRUM-9): generate Spring Boot backend project

---
+++ backend/sim-backend/src/test/java/com/afnan/sim/SimBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afnan.sim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
